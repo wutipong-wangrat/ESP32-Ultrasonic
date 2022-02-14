b78bcc9f75c9bec624d367165a493e5589348b1c
wutipong wangrat <bigbosszaa086@gmail.com>
2022-02-14 16:03:45 +0700

    yes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,51 @@
 #include <Arduino.h>
+const int trigPin = 19;
+const int echoPin = 18;
+const int led = 23; //D23
 
-void setup() {
-  // put your setup code here, to run once:
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+long duration;
+float distanceCm;
+float distanceInch;
+
+void setup(){
+  Serial.begin(115200);     // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED / 2;
+
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
+  if (distanceCm < 10){
+    digitalWrite(led, HIGH);
+    Serial.println("Led: ON ");
+  }else{
+    digitalWrite(led, LOW);
+    Serial.println("Led: OFF ");
+  }
+
+  delay(1000);
 }
